8ab821e13eae7c7d9f6a8ede75395e0f8448d948
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-08-12 13:04:35 -0700

    Almost MVP

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 
+<ProductContext.Provider value={{ products, addItem }}>
+	<CartContext.Provider value={{ cart }}>
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
@@ -20,14 +24,7 @@ function App() {
 			<Navigation cart={cart} />
 
 			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
+			<Route exact path="/" component={Products} />
 				)}
 			/>
 
@@ -38,5 +35,6 @@ function App() {
 		</div>
 	);
 }
-
+</CartContext.Provider>
+</ProductContext.Provider>
 export default App;
